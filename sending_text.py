72f4a48b0343b0d0b53a488103652f7c37f7e0a1
Zach.Golkhou <golkhou@gmail.com>
2020-01-10 01:39:12 -0800

    Create sending_text.py

---
+++ sending_text.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun 13 16:12:24 2013
+
+@author: Zach
+"""
+
+import smtplib
+
+SUBJECT = "Test email from Python"
+fromaddr = 'emial_address'  
+toaddrs  = 'phone_number@vtext.com'  
+#msg = 'Hi! The processing is done!'  
+msg = 'The code is still running!'
+  
+# Credentials (if needed)  
+username = 'username'  
+password = 'password'  
