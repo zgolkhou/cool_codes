77f5eaf816d7a334c5d63ac55517834b5948ce00
Zach.Golkhou <golkhou@gmail.com>
2020-01-10 01:41:25 -0800

    Update sending_text.py

--- sending_text.py
+++ sending_text.py
@@ -45,4 +45,7 @@ msg['To'] = you
 
 # Send the message via our own SMTP server, but don't include the
 # envelope header.
-
+s = smtplib.SMTP('localhost')
+s.sendmail(me, [you], msg.as_string())
+s.quit()
+"""
